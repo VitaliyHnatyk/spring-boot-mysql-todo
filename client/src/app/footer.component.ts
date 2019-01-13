98044948398dae3f7c0b0968778e041260777583
im <vitaliy.hnatyk@gmail.com>
2019-01-13 13:50:17 +0200

    rename extention from css to scss

--- client/src/app/footer.component.ts
+++ client/src/app/footer.component.ts
@@ -3,7 +3,7 @@ import {Component, Input} from '@angular/core';
 @Component({
   template: '<div class="footer"  ><h5>Copyright 2019 The Options Lab {{version}}</h5></div>',
   selector: 'footer',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.scss']
 })
 
 export class FooterComponent {
